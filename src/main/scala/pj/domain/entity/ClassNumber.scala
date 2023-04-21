b730e564e1885e0cb22c797b87078cb0a11787c4
Ana Paula Rabelo <prabelo79@gmail.com>
2023-04-21 17:17:54 +0100

    feat: Implement aircraft class number trait

---
+++ src/main/scala/pj/domain/entity/ClassNumber.scala
@@ -0,0 +1,21 @@
+package pj.domain.entity
+
+sealed trait ClassNumber 
+
+/* 1 - Landing and Small */
+case object Class1 extends ClassNumber
+
+/* 2 - Landing and Large */
+case object Class2 extends ClassNumber
+
+/* 3 - Landing and Heavy */
+case object Class3 extends ClassNumber
+
+/* 4 - Take-off and Small */
+case object Class4 extends ClassNumber
+
+/* 5 - Take-off and Large */
+case object Class5 extends ClassNumber
+
+/* 6 - Take-off and Heavy */
+case object Class6 extends ClassNumber

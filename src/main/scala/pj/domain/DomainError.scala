4b0de2c80af1ced10967ff223e6fb29eb3f0396c
RegianaBarbosa <regiana.cruz@acad.ifma.edu.br>
2023-04-22 01:12:40 +0100

    feat: Update DomainError
    Added InvalidPositiveInteger and InvalidNonNegativeInteger

--- src/main/scala/pj/domain/DomainError.scala
+++ src/main/scala/pj/domain/DomainError.scala
@@ -5,3 +5,5 @@ type Result[A] = Either[DomainError,A]
 enum DomainError:
   case IOFileProblem(error: String)
   case XMLError(error: String)
+  case InvalidPositiveInteger(e: Integer)
+  case InvalidNonNegativeInteger(e: Integer)

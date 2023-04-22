cc4feb164636ec0f535ef8c47377d2221302db07
RegianaBarbosa <regiana.cruz@acad.ifma.edu.br>
2023-04-22 01:08:29 +0100

    feat: Create simple types
    types: PositiveInteger and NonNegativeInteger

---
+++ src/main/scala/pj/domain/SimpleTypes.scala
@@ -0,0 +1,23 @@
+package pj.domain
+
+import DomainError.*
+import scala.annotation.targetName
+
+// Define simple types for positiveInteger and nonNegativeInteger
+object SimpleTypes :
+
+    type Result[A] = Either[DomainError, A]
+
+    opaque type positiveInteger = Int
+    object positiveInteger:
+        def from(value: Int): Result[positiveInteger] =
+            if(value > 0) Right(value) else Left(InvalidPositiveInteger(value))
+        
+        def to(value: positiveInteger): Int = value
+    
+    opaque type nonNegativeInteger = Int
+    object nonNegativeInteger:
+        def from(value: Int): Result[nonNegativeInteger] =
+            if(value >= 0) Right(value) else Left(InvalidNonNegativeInteger(value))
+        
+        def to(value: nonNegativeInteger): Int = value

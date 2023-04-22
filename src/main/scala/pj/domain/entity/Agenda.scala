fbc136020ce94a16101535f351ecd75c52bb5fe6
RegianaBarbosa <regiana.cruz@acad.ifma.edu.br>
2023-04-22 01:13:20 +0100

    feat: Add simpleTypes

--- src/main/scala/pj/domain/entity/Agenda.scala
+++ src/main/scala/pj/domain/entity/Agenda.scala
@@ -1,8 +1,9 @@
 package pj.domain.entity
 
+import pj.domain.SimpleTypes.*
 /**
  * Represents the Aircrafts to operate and available Runways.
  * 
  * It has a list of aircrafts, a list of runways and a maximum delay time.
  */
-final case class Agenda(aircrafts: List[Aircraft], runways: List[Runway], maximumDelayTime: Int)
+final case class Agenda(aircrafts: List[Aircraft], runways: List[Runway], maximumDelayTime: positiveInteger)
--- src/main/scala/pj/domain/entity/Aircraft.scala
+++ src/main/scala/pj/domain/entity/Aircraft.scala
@@ -1,9 +1,10 @@
 package pj.domain.entity
 
+import pj.domain.SimpleTypes.*
 /**
  * Represents the plane
  * 
  * It has an identifier, a class number, and the target time for the operation
  * An aircraft can issue an emergency, which reduces its maximum delay time
  */
-final case class Aircraft(id: String, classNumber: ClassNumber, targetTime: Long, emergency: Option[Int])
+final case class Aircraft(id: String, classNumber: ClassNumber, targetTime: Long, emergency: Option[positiveInteger])

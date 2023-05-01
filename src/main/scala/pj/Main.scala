766a0f505639735f85a55c2cbba08f6edffc6782
Ana Paula Rabelo <prabelo79@gmail.com>
2023-05-01 16:48:22 +0100

    feat: add domain errors

--- src/main/scala/pj/Main.scala
+++ src/main/scala/pj/Main.scala
@@ -1,10 +1,10 @@
-import pj.io.readAgenda
-import pj.domain.schedule.ScheduleMS01
+import pj.io.AgendaIO.*
+import pj.domain.schedule.ScheduleAircrafts.*
 
 object Main:
   def main(args: Array[String]): Unit =
-    val agendaResult = readAgenda("files/assessment/ms01/valid00_in.xml")
+    val agendaResult = readAgenda("files/assessment/ms01/valid02_in.xml")
     
     agendaResult match
         case Left(error) => println(s"$error")
-        case Right(agenda) => ScheduleMS01.scheduleAgenda(agenda)
+        case Right(agenda) => scheduleAgenda(agenda)
--- src/main/scala/pj/domain/DomainError.scala
+++ src/main/scala/pj/domain/DomainError.scala
@@ -3,7 +3,15 @@ package pj.domain
 type Result[A] = Either[DomainError,A]
 
 enum DomainError:
+
   case IOFileProblem(error: String)
   case XMLError(error: String)
   case InvalidPositiveInteger(e: Integer)
   case InvalidNonNegativeInteger(e: Integer)
+  case InvalidClassNumber(e: String)
+  case InvalidId(e: String)
+  case OperationError(e: String)
+  case ScheduleError(e: String)
+  case NoRunwaysAvailable(i: Integer)
+  case RepeatedAircraftId(id: String)
+  case MaximumTimeWindowExceeded(id: String, time: Long)
